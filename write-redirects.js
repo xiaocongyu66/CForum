759fd6917775055edad8af97c7391a71b28d441f
xiaocongyu66 <xiaocongyu1@qq.com>
2026-04-06 18:54:30 +0800

    dev5

---
+++ write-redirects.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const redirects = [
+  '# SPA routing',
+  '/profile /profile.html 200',
+  '/emoji-plaza /emoji-plaza.html 200',
+  '/sections /sections.html 200',
+  '/notifications /notifications.html 200',
+  '/post /post.html 200',
+  '/settings /settings.html 200',
+  '/admin /admin.html 200',
+  '/login /login.html 200',
+  '/register /register.html 200',
+  '/forgot /forgot.html 200',
+  '/reset /reset.html 200',
+].join('\n');
+fs.writeFileSync('public/_redirects', redirects);
+console.log('_redirects written');
